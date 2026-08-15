fbe5cc07b28e1d8bb04c847013ad346c09436fa2
Gregory Row <rowgregory@gmail.com>
2026-08-14 20:03:40 -0400

    chore:add tru host to auth config

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -3,8 +3,6 @@ import { getClosings } from '@/lib/actions/closing/getClosings'
 import { getPageBySlugClient } from '@/lib/actions/page/getPageBySlugClient'
 import { Suspense } from 'react'
 
-export const dynamic = 'force-dynamic'
-
 export default async function ContactUsPage() {
   const [closings, pageData] = await Promise.all([getClosings(), getPageBySlugClient('contact')])
   return (
